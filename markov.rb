57a45646f15ab8d306a673d94f856bcdb53a700f
ahirschberg <alexhirschberg97@gmail.com>
2016-09-09 15:22:27 -0400

    Updated markov with generation loop, added piazza markov database

--- markov.rb
+++ markov.rb
@@ -13,19 +13,20 @@ end
 emails = Mail.find(:what => :first, :count => 10, :order => :asc)
 emails.select! {|mail| mail.from.any? {|s| s =~ /piazza\.com$/}}
 
-puts "no new mail :("; exit if emails.empty?
+puts "no new mail :(" if emails.empty?
 
 piazza_strs = emails.map do |mail|
   # pulls out the post between the piazza boilerplate
   match = mail.body.decoded.match(/^.*?posted.*?\n{2}(.*?)\n{2}Go to http/m)
   match[1] unless match.nil?
 end
-p piazza_strs
 
-markov = MarkyMarkov::TemporaryDictionary.new
+markov = MarkyMarkov::Dictionary.new('piazza_v1')
 piazza_strs.each do |str|
-  markov.parse_string str
+  markov.parse_string str unless str.nil?
 end
-
-10.times {puts markov.generate_1_sentences}
-pry
+markov.save_dictionary!
+while true
+  puts markov.generate_3_sentences
+  sleep 5
+end
